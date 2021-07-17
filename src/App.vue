55bc968efa480d509ccdc92f2980b4382b397610
mat3usps <mateusp.s@outlook.com>
2021-07-17 15:03:51 -0300

    adicionado: fonte do projeto

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,44 @@
 <template>
     <v-app>
-    <CardView />
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
+        <NavAppBar />
+    </v-app>
 </template>
 
 <script>
-import  CardView  from './components/CardView.vue'
+import NavAppBar from "./components/NavAppBar.vue";
 
-export default({
-  name:'App',
-  components: {
-    CardView
-  },
-  setup() {
-    
-  },
-})
+export default {
+    name: "App",
+    components: {
+        NavAppBar,
+    },
+};
 </script>
 
-
 <style lang="scss">
+@font-face {
+    font-family: "GillSansUltraBold";
+    src: local("GillSansUltraBold"), url(./assets/fonts/GillSansUltraBold/GillSansUltraBold.ttf) format("truetype");
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: GillSansUltraBold, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
+    padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+    a {
+        font-weight: bold;
+        color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
+        &.router-link-exact-active {
+            color: #42b983;
+        }
     }
-  }
 }
 </style>
