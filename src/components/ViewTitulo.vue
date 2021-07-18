2135f3e2bddbbb6a3d042a1000c6b7460acdbca3
mat3usps <mateusp.s@outlook.com>
2021-07-17 23:36:15 -0300

    adicionado: Componente ViewTitulo

---
+++ src/components/ViewTitulo.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="view-titulo">
+        <img :src="src" :alt="titulo" />
+        <h1>{{ titulo }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ViewTitulo",
+    props: {
+        src: String,
+        titulo: String,
+    },
+};
+</script>
+
+<style scoped>
+.view-titulo {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+img {
+    border-radius: 50%;
+    height: 100px;
+    width: 100px;
+}
+h1 {
+    position: absolute;
+    background-color: rgb(255, 255, 255, 0.5);
+    padding: 5px;
+    border-radius: 7px;
+}
+</style>
--- src/views/Entretenimento.vue
+++ src/views/Entretenimento.vue
@@ -1,58 +1,63 @@
 <template>
-  <v-main v-if="dadosProfissionais">
-    <CardView 
-      :imagem="dadosProfissionais[0].imagem"
-      :nome="dadosProfissionais[0].nome"
-      :servicos="dadosProfissionais[0].servicos"
-    />
+    <div>
+        <ViewTitulo :src="imagem" titulo="Entretenimento" />
 
-    <CardView 
-      :imagem="dadosProfissionais[5].imagem"
-      :nome="dadosProfissionais[5].nome"
-      :servicos="dadosProfissionais[5 ].servicos"
-    />
+        <div v-if="dadosProfissionais">
+            <CardView
+                :imagem="dadosProfissionais[0].imagem"
+                :nome="dadosProfissionais[0].nome"
+                :servicos="dadosProfissionais[0].servicos"
+            />
 
-    <CardView 
-      :imagem="dadosProfissionais[6].imagem"
-      :nome="dadosProfissionais[6].nome"
-      :servicos="dadosProfissionais[6].servicos"
-    />
+            <CardView
+                :imagem="dadosProfissionais[5].imagem"
+                :nome="dadosProfissionais[5].nome"
+                :servicos="dadosProfissionais[5].servicos"
+            />
 
-    <CardView 
-      :imagem="dadosProfissionais[8].imagem"
-      :nome="dadosProfissionais[8].nome"
-      :servicos="dadosProfissionais[8].servicos"
-    />
-  </v-main>
+            <CardView
+                :imagem="dadosProfissionais[6].imagem"
+                :nome="dadosProfissionais[6].nome"
+                :servicos="dadosProfissionais[6].servicos"
+            />
+
+            <CardView
+                :imagem="dadosProfissionais[8].imagem"
+                :nome="dadosProfissionais[8].nome"
+                :servicos="dadosProfissionais[8].servicos"
+            />
+        </div>
+    </div>
 </template>
 
 <script>
+import ViewTitulo from "../components/ViewTitulo.vue";
+import Entretenimento from "../assets/imagens/entretenimento.jpg";
 import CardView from "../components/CardView.vue";
 
 export default {
-  async mounted() {
-    this.dadosProfissionais = await this.getData();
-    console.log(this.dadosProfissionais);
-  },
-  components: {
-    CardView,
-  },
-  methods: {
-    async getData() {
-      const response = await fetch(
-        "https://it3-gdf-default-rtdb.firebaseio.com/saude/profissionais.json"
-      );
-
-      const data = await response.json();
-
-      return data;
+    async mounted() {
+        this.dadosProfissionais = await this.getData();
+        console.log(this.dadosProfissionais);
     },
-  },
-  data() {
-    return {
-      dadosProfissionais: [],
-      
-    };
-  },
+    components: {
+        CardView,
+        ViewTitulo,
+    },
+    methods: {
+        async getData() {
+            const response = await fetch("https://it3-gdf-default-rtdb.firebaseio.com/saude/profissionais.json");
+
+            const data = await response.json();
+
+            return data;
+        },
+    },
+    data() {
+        return {
+            imagem: Entretenimento,
+            dadosProfissionais: [],
+        };
+    },
 };
 </script>
